b8043ea5f1757635b51d306b4b75d358fe332d07
Stephanie-itslp <64505753+Stephanie-itslp@users.noreply.github.com>
2023-09-29 16:57:43 -0600

    prog2 y prog3

---
+++ Unidad2/programa2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace programa2 
+{ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "Stephanie ", apellidos = "Cordero Mtz", completo;
+            completo = nombre + apellidos;
+            int a = 5, b = 23, suma;
+            suma = a + b;
+            Console.WriteLine(" * * Bienvenido * * " + completo + " * *");
+            Console.Write("La operación suma de A=" + a + " B=" + b);
+            Console.Write($"Tu resultado es {suma}");
+            Console.WriteLine(" -- Gracias :)");
+        }
+    }
+}
---
+++ Unidad2/programa3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace programa3 
+{
+    internal class promedio
+    {
+        static void Main(string[] args)
+        {
+            int c1, c2, c3, c4, c5, c6;
+            float promedio;
+            Console.WriteLine("Escribe Calificacion 1: ");
+            c1 = Convert.ToInt32( Console.ReadLine() );
+            Console.WriteLine("Escribe Calificacion 2: ");
+            c2 = Convert.ToInt32( Console.ReadLine() );
+            Console.WriteLine("Escribe Calificacion 3: ");
+            c3 = Convert.ToInt32( Console.ReadLine() );
+            Console.WriteLine("Escribe Calificacion 4: ");
+            c4 = Convert.ToInt32( Console.ReadLine() );
+            Console.WriteLine("Escribe Calificacion 5: ");
+            c5 = Convert.ToInt32( Console.ReadLine() );
+            Console.WriteLine("Escribe Calificacion 6: ");
+            c6 = Convert.ToInt32( Console.ReadLine() );
+            //PASO- CALCULA PROMEDIO
+            promedio=( c1 + c2 + c3 + c4 + c5 + c6 ) / 6;
+            //PASO- IMPRIME EL RESULTADO
+            Console.WriteLine("Tu promedio es: "+ promedio);
+        }
+    }
+}
