43c3d2c5a1e7d0b62e117bd3c7f2472ef3462295
Stephanie-itslp <64505753+Stephanie-itslp@users.noreply.github.com>
2023-09-29 16:48:02 -0600

    Mi primer programa

---
+++ Unidad2/programa1.cs
@@ -0,0 +1,21 @@
+﻿//comentarios en una línea
+/* Comentarios en bloque:
+ * Console.WriteLine es para imprimir
+ * Para imprimir texto sin variable
+ * tiene que ir entre " " comillas doble */
+using System; //Paso 1
+
+namespace P_uno //Paso 1
+{
+    internal class Program //Paso 1
+    {
+        static void Main(string[] args) //Paso 1
+        {
+            //DECLARAR VARIABLES
+            string nombre = "Stephanie Cordero"; //PASO 2
+            //Imprimir nombre referenciado a una variable
+            Console.WriteLine(nombre); //PASO 3
+            //Cada línea debe finalizalizar con ; punto y coma
+        } //PASO 4
+    } //PASO 4
+} //PASO 4
