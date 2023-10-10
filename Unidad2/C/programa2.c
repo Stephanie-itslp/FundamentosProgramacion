febe606a3f39d90ac610ea119a81be3eee16825d
Stephanie-itslp <64505753+Stephanie-itslp@users.noreply.github.com>
2023-10-09 19:46:53 -0600

    programa C

---
+++ Unidad2/C/programa2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,suma;
+    printf("Escribe el valor de A");
+    scanf("%d", &a);
+    printf("Escribe el valor de B");
+    scanf("%d", &b);
+    suma = a+b;
+    printf("Resultado: %d",suma);
+}
